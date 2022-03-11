5005e6cbfa45dc040cb8c8854d6288fa469068bc
Rawy <aelrawy29@gmail.com>
2022-03-11 19:21:59 +0200

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/components/nav/nav.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { RegisterComponent } from './modules/auth/register/register.component';
+import { ProfileComponent } from './modules/user/profile/profile.component';
+import { ItemDetailsComponent } from './modules/app/item-details/item-details.component';
+import { ItemsComponent } from './modules/app/items/items.component';
+import { CreateItemComponent } from './modules/app/create-item/create-item.component';
+import { HomeComponent } from './modules/app/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    ItemDetailsComponent,
+    ItemsComponent,
+    CreateItemComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
