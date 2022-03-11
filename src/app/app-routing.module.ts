a9fdf8bce5b8301628e125b8126934abd1f2bde4
Rawy <aelrawy29@gmail.com>
2022-03-11 20:40:55 +0200

    auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/app/home/home.component';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { RegisterComponent } from './modules/auth/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     component:HomeComponent
+  },
+  {
+    path: 'register',
+    component:RegisterComponent
+  },
+  {
+    path: 'login',
+    component:LoginComponent
   }
 ];
 
